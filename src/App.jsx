cb40d600277193c15bfde1c625b55a6847171463
LuizHMAL <leite.luizhenrique94@gmail.com>
2025-05-01 16:22:40 -0300

    FIrst commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import Navbar from './layout/NavBar'
+
+function App() {
+  return (
+    <div className='App'>
+   <Navbar/>
+   </div>
+   )
+}
+
+export default App
---
+++ src/layout/NavBar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Navbar(){
+    return (
+        <div>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-success">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Cad Springboot and React</a>
+                    <button 
+                        className="navbar-toggler"
+                        type="button" data-bs-toggle="collapse" 
+                        data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false" 
+                        aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <button 
+                        className="btn bt-outline-light"
+                    >
+                    New User</button>
+
+
+                </div>
+            </nav>
+        </div>
+  
+
+
+   );
+};
